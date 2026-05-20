dfc6a5636b458f099c329cc10e1d88fcf4276b31
kmunoz-pepe <kevynmunozm@est.uepmrmp.edu.ec>
2026-05-20 09:51:36 -0500

    Add files via upload

---
+++ Unidad 1/Variables/Variable.js
@@ -0,0 +1,11 @@
+//let para crear variable queno va a cambiar
+//const para crear una variable que es constante
+//var realiza las dos cosas 
+
+let nombre="kevyn";
+console.log(nombre)
+//console sirve para imprimir por consola 
+const val1=1
+// const no se puede cambiar a futuro 
+console.log(val1)
+val1=2
